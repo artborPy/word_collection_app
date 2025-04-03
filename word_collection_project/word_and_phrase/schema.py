9b9cbcb696f1302061a8858d98fe42812a8b01b6
artur.borkowski <artur.borkowski.fullstack@gmail.com>
2025-04-03 16:01:34 +0200

    added mutations for student create and update

--- word_collection_project/word_and_phrase/schema.py
+++ word_collection_project/word_and_phrase/schema.py
@@ -2,11 +2,51 @@ import graphene
 
 from graphene_django import DjangoObjectType
 from word_and_phrase import models
+from word_and_phrase.models import Student
 
 
 class StudentType(DjangoObjectType):
     class Meta:
         model = models.Student
+
+class CreateStudent(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+        active = graphene.Boolean(required=True)
+
+    student = graphene.Field(StudentType)
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        student = Student.objects.create(
+            student_name=kwargs['name'],
+            student_email=kwargs['email'],
+            student_password=kwargs['password'],
+            student_active=kwargs['active'])
+
+        return CreateStudent()
+
+class UpdateStudent(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        name = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+        active = graphene.Boolean(required=True)
+
+    student = graphene.Field(StudentType)
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        student = Student.objects.get(pk=kwargs['id'])
+        student.student_name = kwargs['name']
+        student.student_email = kwargs['email']
+        student.student_password = kwargs['password']
+        student.student_active = kwargs['active']
+        student.save()
+
 
 class WordType(DjangoObjectType):
     class Meta:
@@ -36,10 +76,14 @@ class Query(graphene.ObjectType):
     def resolve_all_words(root, info, **kwargs):
         return models.Word.objects.all()
 
-    def resolve_words_by_student(root, info, id):
-        return models.Word.objects.filter(student_id=student_id)
+    def resolve_words_by_student(root, info, studentId):
+        return models.Word.objects.filter(student_id=studentId)
+
+
+class Mutation(graphene.ObjectType):
+    create_student = CreateStudent.Field()
+    update_student = UpdateStudent.Field()
 
 
 
-
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
